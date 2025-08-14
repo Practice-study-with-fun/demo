983d8b1c3ccaee2d3e0f5a902b9222d2ac995d99
Bipul <devbtes06@gmail.com>
2025-08-14 21:59:40 +0600

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  module.exports = {
+  env: {
+    BUILD_PHASE: process.env.BUILD_PHASE,
+  },
+};
   /* config options here */
 };
 
